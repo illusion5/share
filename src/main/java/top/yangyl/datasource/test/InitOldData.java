de88c92d765c1aa4f03c89d5f6ca99f7584baf84
yangyanglei <yangyanglei@ebnew.com>
2018-08-02 16:45:33 +0800

    修改逻辑

--- src/main/java/top/yangyl/datasource/test/InitOldData.java
+++ src/main/java/top/yangyl/datasource/test/InitOldData.java
@@ -1,6 +1,6 @@
 package top.yangyl.datasource.test;
 
-import org.springframework.util.CollectionUtils;
+import com.alibaba.fastjson.JSON;
 
 import javax.sql.DataSource;
 import java.util.ArrayList;
@@ -10,7 +10,7 @@ import java.util.Map;
 public class InitOldData {
 
     private static String sql="SELECT supplier_id FROM purchase_supplier_project GROUP BY supplier_id";
-    private static String userSql="select COMPANY_ID,LOGIN_NAME FROM t_reg_user WHERE COMPANY_ID=?";
+    private static String userSql="select COMPANY_ID,LOGIN_NAME FROM t_reg_user WHERE COMPANY_ID in (";
     private static String updateSql="update purchase_supplier_project set supplier_login_name=? where supplier_id=?";
 
     private static DataSource purchaseDataSource=DataSourceUtils.getPurchaseDataSource();
@@ -20,22 +20,17 @@ public class InitOldData {
     public static void main(String[] args) {
 
         List<Map<String, Object>> query = DBUtils.query(purchaseDataSource, sql, null);
-        for (Map map:query) {
+        StringBuilder sb=new StringBuilder();
+        query.forEach(map->sb.append(map.get("supplier_id")).append(","));
+        userSql=userSql+sb.toString().substring(0,sb.length()-1)+")";
+        List<Map<String, Object>> userList= DBUtils.query(userDataSource, userSql, null);
+        System.out.println(JSON.toJSONString(userList));
+        userList.forEach(user->{
             List list=new ArrayList();
-            list.add(map.get("supplier_id"));
-            List<Map<String, Object>> result= DBUtils.query(userDataSource, userSql, list);
-            if(CollectionUtils.isEmpty(result)){
-                continue;
-            }
-            list.remove(0);
-            list.add(result.get(0).get("LOGIN_NAME"));
-            list.add(result.get(0).get("COMPANY_ID"));
+            list.add(user.get("LOGIN_NAME"));
+            list.add(user.get("COMPANY_ID"));
             System.out.println(DBUtils.execute(purchaseDataSource,updateSql, list));
-        }
-
-
-
-
+        });
     }
 
 
